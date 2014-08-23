e3714f7d7d575a0f4b77854c1aa6c6d8a9721890
Byrdann Fox <mobileupstate@gmail.com>
2014-08-23 11:01:15 -0400

    Updates...

--- ECAQuiz/ECAQuiz/ViewController.swift
+++ ECAQuiz/ECAQuiz/ViewController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    var currentQuestionIndex: Int?
+    
+    var questions: NSArray?
+    
+    var answers: NSArray?
+    
+    @IBOutlet weak var showAnswer: UIButton!
+    
+    @IBOutlet weak var showQuestion: UIButton!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBAction func showQuestion(sender: AnyObject) {
+    
+        
+    
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         // Do any additional setup after loading the view, typically from a nib.
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +48,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
